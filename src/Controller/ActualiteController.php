6abdad71685a3b85133325fa1ec3a2d867e92396
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-02-11 02:05:44 +0100

    updatehadhemi2

---
+++ src/Controller/ActualiteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActualiteController extends AbstractController
+{
+    #[Route('/actualite', name: 'app_actualite')]
+    public function index(): Response
+    {
+        return $this->render('actualite/index.html.twig', [
+            'controller_name' => 'ActualiteController',
+        ]);
+    }
+
+    #[Route('/listearticle', name: 'app_listearticle')]
+    public function listearticle(): Response
+    {
+        return $this->render('actualite/list.html.twig', [
+            'controller_name' => 'ActualiteController',
+        ]);
+    }
+    
+    #[Route('/addarticle', name: 'app_addarticle')]
+    public function addarticle(): Response
+    {
+        return $this->render('actualite/add.html.twig', [
+            'controller_name' => 'ActualiteController',
+        ]);
+    }
+}
